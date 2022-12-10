6767cb575534fa9142855e39a7b1307922f51fe4
philippgie <philipp.giersfeld@t-online.de>
2022-12-10 21:29:22 +0200

    1.5: course information step5

--- part1/src/App.js
+++ part1/src/App.js
@@ -6,11 +6,11 @@ const Header = (props) => {
 
 
 const Part = (props) => {
-        return (
-                <>
-                <p>{props.part.name} {props.part.exercises}</p>
-                </>
-               )
+    return (
+            <>
+            <p>{props.part.name} {props.part.exercises}</p>
+            </>
+           )
 }
 
 const Content = (props) => {
@@ -30,29 +30,32 @@ const Total = (props) => {
 }
 
 const App = () => {
-    const course = 'Half Stack application development'
-        const parts = [
-        {
-name: 'Fundamentals of React',
-          exercises: 10
-        }
-    , {
-name: 'Using props to pass data',
-          exercises: 7
-    }
-    , {
-name: 'State of a component',
-          exercises: 14
-    }
-        ]
+    const course = {
 
-            return (
-                    <div>
-                    <Header course={course} />
-                    <Content parts={parts}/>
-                    <Total parts={parts}/>
-                    </div>
-                   )
+name: 'Half Stack application development',
+      parts : [
+      {
+name: 'Fundamentals of React',
+      exercises: 10
+      }
+      , {
+name: 'Using props to pass data',
+      exercises: 7
+      }
+      , {
+name: 'State of a component',
+      exercises: 14
+      }
+      ]
+    }
+
+    return (
+            <div>
+            <Header course={course.name} />
+            <Content parts={course.parts}/>
+            <Total parts={course.parts}/>
+            </div>
+           )
 }
 
 export default App
