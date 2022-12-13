8ca4cff4a695baff74a311d90742f59b007bf24b
philippgie <philipp.giersfeld@t-online.de>
2022-12-13 20:43:08 +0200

    2.14*: Data for countries, step3

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -9,32 +9,33 @@ const App = () => {
 
     useEffect(() => {
         if(newSearch){
-                console.log('effect')
-                axios
-                    .get(`https://restcountries.com/v3.1/name/${newSearch}`)
-                    .then(response => {
-                        console.log('promise fulfilled')
-                        setNewCountries(response.data)
-                    })
-                .catch(function (error){
-                        if(error.response.status!==404)
-                            console.log(error)
-                        else
-                            setNewCountries([])
+            console.log('effect')
+            axios
+                .get(`https://restcountries.com/v3.1/name/${newSearch}`)
+                .then(response => {
+                    console.log('promise fulfilled')
+                    setNewCountries(response.data)
                 })
-            }},[newSearch])
+                .catch(function (error){
+                    if(error.response.status!==404)
+                        console.log(error)
+                    else
+                        setNewCountries([])
+                })
+        }
+    },[newSearch])
 
     const handleSearchChange = (event) => {
         console.log(event.target.value)
         setNewSearch(event.target.value)
     }
 
-        return (
-            <div>
-                <Filter state={newSearch} handler={handleSearchChange}/>
-                <Display countries={countries} setSearch={setNewSearch}/>
-            </div>
-        )
+    return (
+        <div>
+            <Filter state={newSearch} handler={handleSearchChange}/>
+            <Display countries={countries} setSearch={setNewSearch}/>
+        </div>
+    )
 }
 
 
--- part2/countries/src/components/File.js
+++ part2/countries/src/components/File.js
@@ -1,3 +1,6 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
 const Filter = ({state, handler}) => {
     return (
         <>
@@ -9,11 +12,11 @@ const Display = ({countries, setSearch}) => {
     console.log(countries.length)
     if(countries.length===1)
         return <OneCountry country={countries[0]}/>
-    else if(countries.length===0)
+            else if(countries.length===0)
         return (<p>No matches</p>)
     else if(countries.length<=10)
         return <Countries countries={countries} setSearch={setSearch}/>
-    else if(countries.length>10)
+            else if(countries.length>10)
         return (<p>Too many matches, specify another filter</p>)
 }
 
@@ -50,15 +53,48 @@ const OneCountry = ({country}) => {
                 {country.languages.length===1 ? <li>{country.language}</li> : Object.entries(country.languages).map(language=><li key={language[1]}>{language[1]}</li>)}
             </ul>
             <Flag png={country.flags.png}/>
-
+            <Weather name={country.capital}/>
         </>
     )
 }
 const Button = (props) => (
-        <button onClick={props.handleClick}>
+    <button onClick={props.handleClick}>
         {props.text}
-        </button>
-        )
+    </button>
+)
+
+const Weather = ({name}) => {
+    const api_key = process.env.REACT_APP_API_KEY
+
+    const [icon, newIcon] = useState(null)
+    const [temp, newTemp] = useState(null)
+    const [wind, newWind] = useState(null)
+
+
+    useEffect(() => {
+        axios
+            .get(`https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${api_key}`)
+            .then(response => {
+                console.log(response.data)
+                newTemp(response.data.main.temp)
+                newWind(response.data.wind.speed)
+                newIcon(response.data.weather[0].icon)
+            })
+            .catch(function (error){
+                console.log(error)
+            })
+    },[])
+
+    console.log(icon)
+    return (
+        <>
+            <h2>Weather in {name}</h2>
+            <p>temperature {temp}</p>
+            <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`}/>
+            <p>wind {wind}</p>
+        </>
+    )
+}
 
 
 //export default {Persons, PersonForm, Filter}
