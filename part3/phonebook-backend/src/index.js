4fa5928bacf433707111e5d7211454126d9dcef0
philippgie <philipp.giersfeld@t-online.de>
2022-12-14 12:09:42 +0200

    3.4: Phonebook backend step6

--- part3/phonebook-backend/src/index.js
+++ part3/phonebook-backend/src/index.js
@@ -36,13 +36,17 @@ const generateId = () => {
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
-    console.log(body)
 
-    //if (!body.content) {
-    //    return response.status(400).json({ 
-    //        error: 'content missing' 
-    //    })
-    //}
+    if (!body.name | !body.number) {
+        return response.status(400).json({ 
+            error: 'missing data' 
+        })
+    }
+    if (persons.some(person => person.name === body.name)) {
+        return response.status(400).json({ 
+            error: 'name must be unique' 
+        })
+    }
 
     const person = {
         name: body.name,
