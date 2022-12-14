2d6389c55c9264e6c627839d5c887dbd921cda6c
philippgie <philipp.giersfeld@t-online.de>
2022-12-14 11:24:52 +0200

    3.2: Phonebook backend step2

--- part3/phonebook-backend/src/index.js
+++ part3/phonebook-backend/src/index.js
@@ -3,84 +3,88 @@ const app = express()
 
 let persons = [
     { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+        "id": 1,
+        "name": "Arto Hellas", 
+        "number": "040-123456"
     },
     { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+        "id": 2,
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
     },
     { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+        "id": 3,
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
     },
     { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+        "id": 4,
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
     }
 ]
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+    res.send('<h1>Hello World!</h1>')
 })
 
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+    return maxId + 1
 }
 
 app.post('/api/persons', (request, response) => {
-  const body = request.body
+    const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
+    if (!body.content) {
+        return response.status(400).json({ 
+            error: 'content missing' 
+        })
+    }
 
-  const note = {
-    content: body.content,
-    //important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
+    const note = {
+        content: body.content,
+        //important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+    }
 
-  notes = notes.concat(note)
+    notes = notes.concat(note)
 
-  response.json(note)
+    response.json(note)
 })
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+    res.json(persons)
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
 
-  response.status(204).end()
+    response.status(204).end()
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = persons.find(person => person.id === id)
+    const id = Number(request.params.id)
+    const note = persons.find(person => person.id === id)
 
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.get('/info', (req, res) => {
+    res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
