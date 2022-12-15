92104fbd5a1546dd8c63e584d0e9ab8879a77eed
philippgie <philipp.giersfeld@t-online.de>
2022-12-15 16:28:47 +0200

    3.12: Command-line database

---
+++ part3/phonebook-backend/src/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password> <name> <number>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://philipp:${password}@cluster1.wrsm1yx.mongodb.net/?retryWrites=true&w=majority`
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number:String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+mongoose
+    .connect(url)
+    .then((result) => {
+        if(process.argv.length===5){
+            const person = new Person({
+                name: process.argv[3],
+                number: process.argv[4],
+            })
+            person.save()
+                .then(result => {
+                    mongoose.connection.close()
+                })
+        }else{
+            Person.find({})
+                .then(result => {
+                    console.log("phonebook:")
+                    result.forEach(person => {
+                        console.log(person.name, person.number)
+                    })
+                    mongoose.connection.close()
+                })
+        }
+    })
+    .catch((err) => console.log(err))
