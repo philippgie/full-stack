18a5936224eb1255c4e38a79466b4d7d8f4baf43
philippgie <philipp.giersfeld@t-online.de>
2022-12-16 21:27:08 +0200

    4.10: Blog list tests, step3

--- part4/part4-blogs-backend/tests/note_api.test.js
+++ part4/part4-blogs-backend/tests/note_api.test.js
@@ -53,6 +53,28 @@ test('identifier property is named id', async () => {
     expect(blog.id).toBeDefined()
 })
 
+test('a valid blog can be added ', async () => {
+  const newBlog = {
+    title: 'async/await simplifies making async calls',
+    author: 'Olli',
+    url: 'olli.com'
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+  const titles = blogsAtEnd.map(n => n.title)
+  expect(titles).toContain(
+    'async/await simplifies making async calls'
+  )
+})
+
 afterAll(() => {
     mongoose.connection.close()
 })
