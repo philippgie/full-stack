8d6a79819b9476cd3229cbb2f94bea013db84632
philippgie <philipp.giersfeld@t-online.de>
2022-12-16 16:30:12 +0200

    4.4: helper functions and unit tests, step2

--- part4/part4-blogs-backend/utils/list_helper.js
+++ part4/part4-blogs-backend/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    return blogs.reduce( function(a, b){
+        return a + b.likes;
+    }, 0);
+}
+
 module.exports = {
-  dummy
+  dummy,
+    totalLikes
 }
