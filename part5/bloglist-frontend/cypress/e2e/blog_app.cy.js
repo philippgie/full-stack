79ec91cc1f3e0b1f9c1cf7b6f781600ec8d396fb
philippgie <philipp.giersfeld@t-online.de>
2022-12-21 18:44:40 +0200

    5.18: bloglist end to end testing, step2

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,13 +1,29 @@
-describe('Blog app', function() {
-  beforeEach(function() {
-    cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    cy.visit('http://localhost:3000')
-  })
 
-  it('Login form is shown', function() {
-      cy.request('POST', 'http://localhost:3003/api/users', {username:'pipo', password:'pass'})
-      cy.get('#username').type('pipo')
-      cy.get('#password').type('pass')
-      cy.contains('login').click()
+describe('Blog app', function() {
+    beforeEach(function() {
+        cy.request('POST', 'http://localhost:3003/api/testing/reset')
+        cy.request('POST', 'http://localhost:3003/api/users', {username:'pipo', password:'pass'})
+        cy.visit('http://localhost:3000')
+    })
+
+    it('Login form is shown', function() {
+        cy.contains('username')
+        cy.contains('password')
+    })
+
+    describe('Login',function() {
+        it('succeeds with correct credentials', function() {
+            cy.get('#username').type('pipo')
+            cy.get('#password').type('pass')
+            cy.contains('login').click()
+            cy.contains('successful authentication')
+        })
+
+        it('fails with wrong credentials', function() {
+            cy.get('#username').type('pass')
+            cy.get('#password').type('pipo')
+            cy.contains('login').click()
+            cy.contains('wrong credentials')
+        })
   })
 })
